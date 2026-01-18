1548e7c40b0ddedf423abdc876db03e0a9fc129f
Timothy Bjorklund <timothybjorklund@outlook.com>
2026-01-17 20:16:20 -0700

    Initial Commit:
    -Create Java Project
    -Create Class
    -Build Task List for Project
    -Created Project Skeleton and tested output
    -Initialize Git local repositories

---
+++ src/M1CT/BagDemo.java
@@ -0,0 +1,44 @@
+package M1CT;
+
+/*
+ * M1CT BagDS - Project Task List
+ *
+ * Setup
+ * // DONE: Confirm project builds and runs (BagDemo main prints a message).
+ * // DONE: Initialize Git Local Repository
+ * // TODO: Initial Commit
+ * // TODO: Link to GitHub and do Initial push
+ *
+ * Bag ADT Implementation
+ * // TODO: Create Bag<T> class.
+ * // TODO: Add internal storage (ArrayList).
+ * // TODO: Implement add(T item).
+ * // TODO: Implement remove(T item) to remove one occurrence if it exists.
+ * // TODO: Implement contains(T item).
+ * // TODO: Implement count(T item).
+ * // TODO: Add a toString() method to print bag contents.
+ *
+ * Demo Requirements
+ * // TODO: Create a Bag instance in main.
+ * // TODO: Add several elements including duplicates.
+ * // TODO: Print bag contents.
+ * // TODO: Test contains() for a few elements.
+ * // TODO: Test count() for a few elements.
+ * // TODO: Remove an element from the bag.
+ * // TODO: Print bag contents again.
+ * // TODO: Test contains() for the removed element.
+ * // TODO: Test count() for the removed element.
+ *
+ * Final Checks
+ * // TODO: Add short method comments explaining purpose and behavior.
+ * // TODO: Write pseudocode that matches the final code.
+ */
+
+public class BagDemo {
+
+	public static void main(String[] args) {
+		System.out.println("Bag demo project setup");
+
+	}
+
+}
